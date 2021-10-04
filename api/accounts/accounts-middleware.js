c698333650a49eb6175ac8e194cac7f7064da216
nhamilton1 <nickh3592@gmail.com>
2021-10-04 17:46:01 -0400

    trying with yup

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,63 +1,65 @@
 const Accounts = require('./accounts-model')
-// const yup = require('yup')
+const yup = require('yup')
 const db = require('../../data/db-config')
 
-// const accountPayloadSchema = yup.object().shape({
-//   name: yup
-//     .string('name of account must be a string')
-//     .trim()
-//     .min(3, 'name of account must be between 3 and 100')
-//     .max(100, 'name of account must be between 3 and 100')
-//     .typeError('name must be a string')
-//     .required(),
-//   budget: yup
-//     .number('budget of account must be a number')
-//     .positive('budget of account is too large or too small')
-//     .typeError('budget bust be a number')
-//     .required()
-// })
-
-// exports.checkAccountPayload = async (req, res, next) => {
-//   try {
-//     const validatedPayload = await accountPayloadSchema.validate(
-//       req.body,
-//       { strict: false, stripUnknown: true }
-//     )
-//     req.body = validatedPayload
-//     next()
-//   } catch (err) {
-//     res.status(400).json({
-//       message: 'name and budget are required'
-//     })
-//   }
-// }
+const accountPayloadSchema = yup.object().shape({
+  name: yup
+    .string('name of account must be a string')
+    .matches(/^[aA-zZ\s]+$/, 'name of account must be a string')
+    .trim()
+    .min(3, 'name of account must be between 3 and 100')
+    .max(100, 'name of account must be between 3 and 100')
+    .typeError('name must be a string')
+    .required('name and budget are required'),
+  budget: yup
+    .number('budget of account must be a number')
+    .positive('budget of account is too large or too small')
+    .lessThan(999999,'budget of account is too large or too small')
+    .typeError('budget must be a number')
+    .required('name and budget are required')
+})
 
 exports.checkAccountPayload = async (req, res, next) => {
-  const { name, budget } = req.body
-  if (name === undefined || budget === undefined) {
-    res.status(400).json({
-      message:'name and budget are required'
-    })
-  } else if (typeof name !== 'string') {
-    res.status(400).json({
-      message:'name of account must be a string'
-    })
-  } else if (name.trim().length < 3 || name.trim().length > 100) {
-    res.status(400).json({
-      message:'name of account must be between 3 and 100'
-    })
-  } else if (typeof budget !== 'number' || isNaN(budget)) {
-    res.status(400).json({
-      message:'budget of account must be a number'
-    })
-  } else if (budget < 0 || budget > 1000000) {
-    res.status(400).json({
-      message:'budget of account is too large or too small'
-    })
-  } else {
+  try {
+    const validatedPayload = await accountPayloadSchema.validate(
+      req.body,
+      { strict: false, stripUnknown: true }
+    )
+    req.body = validatedPayload
     next()
+  } catch (err) {
+    res.status(400).json({
+      message: err.message
+    })
   }
 }
+
+// exports.checkAccountPayload = async (req, res, next) => {
+//   const { name, budget } = req.body
+//   if (name === undefined || budget === undefined) {
+//     res.status(400).json({
+//       message:'name and budget are required'
+//     })
+//   } else if (typeof name !== 'string') {
+//     res.status(400).json({
+//       message:'name of account must be a string'
+//     })
+//   } else if (name.trim().length < 3 || name.trim().length > 100) {
+//     res.status(400).json({
+//       message:'name of account must be between 3 and 100'
+//     })
+//   } else if (typeof budget !== 'number' || isNaN(budget)) {
+//     res.status(400).json({
+//       message:'budget of account must be a number'
+//     })
+//   } else if (budget < 0 || budget > 1000000) {
+//     res.status(400).json({
+//       message:'budget of account is too large or too small'
+//     })
+//   } else {
+//     next()
+//   }
+// }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
   try {
